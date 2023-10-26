6e758251e461e15a0fc82227aac7654ef29b7e1d
Marcel Blijleven <marcelblijleven@gmail.com>
2023-10-26 22:14:05 +0200

    chore: add Plex to GetUsers task

--- pkg/tasks/users.go
+++ pkg/tasks/users.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"fmt"
 	"gobble/pkg/apps"
 	"gobble/pkg/configuration"
 	"gobble/pkg/users"
@@ -12,7 +13,7 @@ func GetUsers(cfg *configuration.Config) ([]users.User, error) {
 	var u []users.User
 
 	// TODO: programmatically retrieve apps instead of manually defining slice of Apps
-	for _, s := range []apps.App{cfg.Jellyfin.App} {
+	for _, s := range []apps.App{cfg.Jellyfin.App, cfg.Plex.App} {
 		su, err := s.GetUsers()
 
 		if err != nil {
@@ -22,5 +23,6 @@ func GetUsers(cfg *configuration.Config) ([]users.User, error) {
 		u = append(u, su...)
 	}
 
+	fmt.Printf("Foo %+v\n", u)
 	return u, nil
 }
