4e04ba30ee49a150c064002674e82782075ac6f7
Marcel Blijleven <marcelblijleven@gmail.com>
2023-10-26 14:50:55 +0200

    refactor: rename service to app

--- pkg/apps/jellyfin/types.go
+++ pkg/apps/jellyfin/types.go
@@ -90,3 +90,67 @@ type schedule struct {
 	StartHour int    `json:"StartHour"`
 	EndHour   int    `json:"EndHour"`
 }
+
+// SystemInfo is the Jellyfin API response for /System/Info
+type SystemInfo struct {
+	LocalAddress               string                  `json:"LocalAddress"`
+	ServerName                 string                  `json:"ServerName"`
+	Version                    string                  `json:"Version"`
+	ProductName                string                  `json:"ProductName"`
+	OperatingSystem            string                  `json:"OperatingSystem"`
+	Id                         string                  `json:"Id"`
+	StartupWizardCompleted     bool                    `json:"StartupWizardCompleted"`
+	OperatingSystemDisplayName string                  `json:"OperatingSystemDisplayName"`
+	PackageName                string                  `json:"PackageName"`
+	HasPendingRestart          bool                    `json:"HasPendingRestart"`
+	IsShuttingDown             bool                    `json:"IsShuttingDown"`
+	SupportsLibraryMonitor     bool                    `json:"SupportsLibraryMonitor"`
+	WebSocketPortNumber        int                     `json:"WebSocketPortNumber"`
+	CompletedInstallations     []completedInstallation `json:"CompletedInstallations"`
+	CanSelfRestart             bool                    `json:"CanSelfRestart"`
+	CanLaunchWebBrowser        bool                    `json:"CanLaunchWebBrowser"`
+	ProgramDataPath            string                  `json:"ProgramDataPath"`
+	WebPath                    string                  `json:"WebPath"`
+	ItemsByNamePath            string                  `json:"ItemsByNamePath"`
+	CachePath                  string                  `json:"CachePath"`
+	LogPath                    string                  `json:"LogPath"`
+	InternalMetadataPath       string                  `json:"InternalMetadataPath"`
+	TranscodingTempPath        string                  `json:"TranscodingTempPath"`
+	SystemArchitecture         string                  `json:"SystemArchitecture"`
+}
+
+// completedInstallation is part of the SystemInfo response
+type completedInstallation struct {
+	Guid        string      `json:"Guid"`
+	Name        string      `json:"Name"`
+	Version     string      `json:"Version"`
+	Changelog   string      `json:"Changelog"`
+	SourceUrl   string      `json:"SourceUrl"`
+	Checksum    string      `json:"Checksum"`
+	PackageInfo packageInfo `json:"PackageInfo"`
+}
+
+// packageInfo is part of the completedInstallation
+type packageInfo struct {
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Overview    string    `json:"overview"`
+	Owner       string    `json:"owner"`
+	Category    string    `json:"category"`
+	Guid        string    `json:"guid"`
+	Versions    []version `json:"versions"`
+	ImageUrl    string    `json:"imageUrl"`
+}
+
+// version is part of the packageInfo
+type version struct {
+	Version        string `json:"version"`
+	VersionNumber  string `json:"VersionNumber"`
+	Changelog      string `json:"changelog"`
+	TargetAbi      string `json:"targetAbi"`
+	SourceUrl      string `json:"sourceUrl"`
+	Checksum       string `json:"checksum"`
+	Timestamp      string `json:"timestamp"`
+	RepositoryName string `json:"repositoryName"`
+	RepositoryUrl  string `json:"repositoryUrl"`
+}
--- pkg/services/service.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import "gobble/pkg/users"
-
-// Service interface defines the public methods each media service should
-// implement
-type Service interface {
-	GetUsers() []users.User
-}
