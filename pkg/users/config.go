ab918a9c48f03ad0b469396089cf83eb2695d784
Marcel Blijleven <marcelblijleven@gmail.com>
2023-10-26 15:03:57 +0200

    chore(users): add config so users can define user mappings

---
+++ pkg/users/config.go
@@ -0,0 +1,18 @@
+package users
+
+import "fmt"
+
+// UserConfig represents user related config file entries
+type UserConfig struct {
+	UserMappings []UserMapping `json:"user_mapping" toml:"user_mapping"`
+}
+
+// UserMapping is part of the UserConfig and allows usernames to be mapped between apps
+type UserMapping struct {
+	From string `json:"from" toml:"from"`
+	To   string `json:"to" toml:"to"`
+}
+
+func (u UserMapping) String() string {
+	return fmt.Sprintf("username %q mapped to %q, and vice versa", u.From, u.To)
+}
