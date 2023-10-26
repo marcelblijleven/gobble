c83395a4c019ade234d8fda8b89e469348d76fb1
Marcel Blijleven <marcelblijleven@gmail.com>
2023-10-26 15:00:58 +0200

    chore(users): extend user struct with Source and MappedUsers

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -1,6 +1,10 @@
 package users
 
+// User is a Gobble user, can be from any map
+// mapped users are also stored on the User object
 type User struct {
-	ID       string
-	Username string
+	ID          string `json:"id"`
+	Username    string `json:"username"`
+	Source      string `json:"source"`
+	MappedUsers []User `json:"mapped_users"`
 }
