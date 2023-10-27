0907d49f1f5cef07c7b4c4bd46dd6ae51e935a2f
Marcel Blijleven <marcelblijleven@gmail.com>
2023-10-27 18:11:02 +0200

    chore: add get identifier method to User struct

--- pkg/users/user.go
+++ pkg/users/user.go
@@ -1,5 +1,9 @@
 package users
 
+import (
+	"fmt"
+)
+
 // User is a Gobble user, can be from any map
 // mapped users are also stored on the User object
 type User struct {
@@ -8,6 +12,16 @@ type User struct {
 	MatchedUsers []MatchedUser `json:"matched_users"`
 	*Source
 }
+
+// GetIdentifier combines the users' Username and Source.ServerID to create a unique
+// identifier per app
+func (u User) GetIdentifier() string {
+	return fmt.Sprintf("%s:%s", u.Username, u.ServerID)
+}
+
+//func (u User) String() string {
+//	return fmt.Sprintf("user %q from service %s with server id %q", u.Username, u.ServerType, u.ServerID)
+//}
 
 // MatchedUser defines the match between two User objects
 type MatchedUser struct {
